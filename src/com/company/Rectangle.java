8e740847526b6e1385731d19af61bd4637bc3467
Karstenheiseldal <karstenheiseldal@outlook.com>
2021-09-28 12:44:41 +0200

    Changed rectangle attributes name to bottomRightX etc, to give it more sense. Happy with the change.

--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -1,14 +1,14 @@
 package com.company;
 
 public class Rectangle extends Shapes {
-    int axRec;
-    int ayRec;
-    int bxRec;
-    int byRec;
-    int cxRec;
-    int cyRec;
-    int dxRec;
-    int dyRec;
+    int bottomRightX;
+    int bottomRightY;
+    int bottomLeftX;
+    int bottomLeftY;
+    int topLeftX;
+    int topLeftY;
+    int topRightX;
+    int topRightY;
 
     int length;
     int width;
@@ -31,19 +31,19 @@ public class Rectangle extends Shapes {
     @Override
     public void createShape() {
 
-            axRec = 5;
-            ayRec = 7;
-            bxRec = 2;
-            byRec = 9;
-            cxRec = 5;
-            cyRec = 9;
-            dxRec = 2;
-            dyRec = 7;
+        bottomRightX = 5;
+        bottomRightY = 7;
+        bottomLeftX = 2;
+        bottomLeftY = 9;
+        topLeftX = 5;
+        topLeftY = 9;
+        topRightX = 2;
+        topRightY = 7;
     }
 
     @Override
     public void validateShape(){
-        if(this.axRec == this.cxRec && this.ayRec == this.dyRec){
+        if(this.bottomRightX == this.topRightX && this.bottomRightY == this.topRightY){
             System.out.println("Nice");
         } else {
             System.out.println("yo");
@@ -54,10 +54,10 @@ public class Rectangle extends Shapes {
     @Override
     public void getShape() {
         System.out.println("Rectangle ABCD has the following coordinates. " +
-                " A=("+axRec+", "+ayRec+")"+
-                " B=("+bxRec+", "+byRec+")"+
-                " C=("+cxRec+", "+cyRec+")"+
-                " D=("+dxRec+", "+dyRec+")");
+                " A=("+bottomRightX+", "+bottomRightY+")"+
+                " B=("+bottomLeftX+", "+bottomLeftY+")"+
+                " C=("+topLeftX+", "+topLeftY+")"+
+                " D=("+topRightX+", "+topRightY+")");
     }
 
     @Override
