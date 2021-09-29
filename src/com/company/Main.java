7a7e80c917c89921f27a70d9448fe9ab487f77ec
Karstenheiseldal <karstenheiseldal@outlook.com>
2021-09-29 10:05:44 +0200

    Planning to store coordinates as arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,40 +5,40 @@ public class Main {
     public static void main(String[] args) {
        //programStart
         Rectangle rec = new Rectangle();  //new rectangle object
-        rec.setName("Oliver"); //give the rectangle a name
-        rec.getName(); //print out the rectangle name
-        rec.createShape(); //sets the coordinates for the rectangle, and prints it after.
+        rec.setName("Oliver");            //give the rectangle a name
+        rec.getName();                    //print out the rectangle name
+        rec.createShape();                //sets the coordinates for the rectangle, and prints it after.
         rec.getShape();
-        rec.calculateShapeArea(); //calculates and returns rectangle area, circumference and centre.
+        rec.calculateShapeArea();         //calculates and returns rectangle area, circumference and centre.
         rec.calculateShapeCircimfurence();
-        rec.getCenter(); //prints centre X and Y coordinates for the rectangle
+        rec.getCenter();                  //prints centre X and Y coordinates for the rectangle
         rec.moveShape(4,5); //move shape to inserted x and y coordinates
-        rec.getCenter(); //to confirm that the object has now been moved by printing centre again
+        rec.getCenter();                 //to confirm that the object has now been moved by printing centre again
 
         System.out.println();
         System.out.println();
 
-        Triangle tri = new Triangle(); //new triangle
-        tri.setName("Julius"); // give the triangle a name.
-        tri.getName(); //print out triangle name
-        tri.createShape(); //sets the coordinates for the triangle
-        tri.getShape(); //prints triangle coordinates (ABC)
-        tri.calculateShapeArea(); //calculating area and circumference
+        Triangle tri = new Triangle();      //new triangle
+        tri.setName("Julius");              //give the triangle a name.
+        tri.getName();                      //print out triangle name
+        tri.createShape();                  //sets the coordinates for the triangle
+        tri.getShape();                     //prints triangle coordinates (ABC)
+        tri.calculateShapeArea();           //calculating area and circumference
         tri.calculateShapeCircimfurence();
-        tri.getCenter(); //prints centre coordinates for the triangle
-        tri.moveShape(2,4);
+        tri.getCenter();                    //prints centre coordinates for the triangle
+        tri.moveShape(2,4);   //moves the triangles collective x and y coordinates
 
         System.out.println();
         System.out.println();
 
-        Circle cir = new Circle();
-        cir.setName("Ole");
-        cir.getName();
-        cir.createShape();
-        cir.getShape();
-        cir.calculateShapeArea();
-        cir.calculateShapeCircimfurence();
-        cir.getCenter();
-        cir.moveShape(1,5);
+        Circle cir = new Circle(); //new circle object
+        cir.setName("Ole");        //name the circle
+        cir.getName();             //print name
+        cir.createShape();         //sets centre x and y coordinates and radius
+        cir.getShape();            //prints the coordinates
+        cir.calculateShapeArea();  //calculates the area of the circle
+        cir.calculateShapeCircimfurence(); //calculates and prints the circle circumference
+        cir.getCenter();                    //prints the centre for the circle
+        cir.moveShape(1,5);    //moves the circle's centre to given X and Y
     }
 }
--- src/com/company/Shapes.java
+++ src/com/company/Shapes.java
@@ -1,16 +1,16 @@
 package com.company;
 
-public abstract class Shapes { //All shapes have to contain following methods
+public abstract class Shapes {              //All shapes have to contain following methods
 
     String name;
     public abstract void setName(String name); //saying all subclasses should use this method for naming objects.
     public abstract void getName();         //the program should be able to print out the name as well.
-    public abstract void createShape(); //giving coordinates to the objects
-    public abstract void getShape(); //a method for revealing the coordinates has to be applied
-    public abstract void  calculateShapeArea();
-    public abstract void  calculateShapeCircimfurence();
-    public abstract void  getCenter();
-    public abstract void moveShape(int moveX, int moveY);
+    public abstract void createShape();     //giving coordinates to the objects
+    public abstract void getShape();        //a method for revealing the coordinates has to be applied
+    public abstract void calculateShapeArea(); //all subclasses should be able to calculate the shape area and circumference and centre.
+    public abstract void calculateShapeCircimfurence();
+    public abstract void getCenter();
+    public abstract void moveShape(int moveX, int moveY); //the shapes can be moved to new coordinates.
 
 }
 
